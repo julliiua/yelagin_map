c63dd3f27c6ff52e9e226f5f16fe82777a911781
julliiua <yuliya.zubulina@gmail.com>
2025-03-19 20:43:36 +0300

    upd

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 // Добавляем точки на карту
 var places = [
     {
-        coords: [59.977, 30.247],
+        coords: [59.976634, 30.268617],
         title: "Елагиноостровский дворец",
         img: "https://upload.wikimedia.org/wikipedia/commons/3/33/Yelagin_Palace_SPB_01.jpg",
         text: "Построен Карло Росси в стиле классицизма, вдохновлён античными ордерами."
@@ -23,19 +23,18 @@ var places = [
 
 // Добавляем маркеры
 places.forEach(place => {
-    let marker = L.circleMarker(place.coords, {
-        color: 'blue',        // Контур синий
-        fillColor: 'lightblue', // Заливка голубая
-        fillOpacity: 0.5,
-        radius: 8
-    }).addTo(map)
-    .bindPopup(`<h3>${place.title}</h3><img src="${place.img || ''}" width="150"><p>${place.text}</p>`);
+    const defaultMarker = L.marker(place.coords).addTo(map)
+        .bindPopup(`<h3>${place.title}</h3><img src="${place.img || ''}" width="150"><p>${place.text}</p>`);
 
-    // При клике на точку меняем цвет на красный
-    marker.on('click', function() {
-        this.setStyle({
-            color: 'red',        // Красный контур
-            fillColor: 'pink'    // Светло-красная заливка
-        });
+    defaultMarker.on('click', function() {
+        // Меняем маркер на красный при клике
+        this.setIcon(L.icon({
+            iconUrl: 'https://cdn-icons-png.flaticon.com/512/929/929596.png', // ссылка на красный маркер
+            iconSize: [25, 41], // размер иконки
+            iconAnchor: [12, 41], // якорь иконки
+            popupAnchor: [1, -34], // позиция всплывающего окна
+            shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png',
+            shadowSize: [41, 41]
+        }));
     });
 });
